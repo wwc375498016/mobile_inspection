c3cd967d2f634931fe7896c9e7f7471ba8f85fb1
Jzsama <1053420493@qq.com>
2019-07-24 18:05:28 +0800

    Revert "2019/7/24 OYJZ" all

---
+++ src/main/java/servlet/GetPic.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import dao.UserDAO;
+import net.sf.json.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "GetPic")
+public class GetPic extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+
+        try (PrintWriter out = response.getWriter()) {
+
+            //获得请求中传来的用户名和密码
+            String picName = request.getParameter("picName").trim();
+            InputStream in = null;
+            byte[] data = null;
+            // 读取图片字节数组
+            in = new FileInputStream("C:/xampp/tomcat/webapps/pic/" +picName+".jpg");
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+            // 对字节数组Base64编码
+            String encoder = Base64.encodeBase64String(data);
+            // 返回Base64编码过的字节数组字符串
+            Map<String, String> params = new HashMap<>();
+            JSONObject jsonObject = new JSONObject();
+            params.put("Code", encoder);
+            jsonObject.put("params", params);
+            out.write(jsonObject.toString());
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
